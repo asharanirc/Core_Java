bc4b8e32c32c6ac36e43c394255bcf0d2d76843c
asharanirc <152201103+asharanirc@users.noreply.github.com>
2023-11-27 21:13:20 -0800

    Add files via upload

---
+++ setter and getter/src/Thursday/Practice.java
@@ -0,0 +1,12 @@
+package Thursday;
+
+public class Practice {
+int x=10;
+int y=20;
+
+public void additionOfTwoNumber() {
+
+	int c=x+y;
+	System.out.println();
+}
+}
---
+++ setter and getter/src/com/tech/setter/Demo.java
@@ -0,0 +1,11 @@
+package com.tech.setter;
+
+public class Demo {
+  int x=10;
+  int y=20;
+  public void additionOfTwoNumber()
+  {
+	  System.out.println();
+	  
+  }
+}
